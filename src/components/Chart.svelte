d98684d8c26b86adb8733fb9933eee83e13264a0
A.Koenig <stfu.pls@web.de>
2022-01-24 17:37:40 +0100

    changed topics and added link to our GitHub

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -174,7 +174,7 @@
     {/if}
   {:else}
     <div class="informationen">
-      <h2>Warum?</h2>
+      <h2>Warum gibt es diese Seite?</h2>
 
       <p class="infotxt">Diese Website entstand im Rahmen des Kurses <strong>GDV</strong> (<em
         >Grundlagen der Datenvisualisierung</em
@@ -188,13 +188,13 @@
       <strong>Luftqualität</strong>
       zu untersuchen, insbesondere die Entwicklung seit Ausbruch der Coronapandemie.</p>
 
-      <h2>Was?</h2>
+      <h2>Welche Daten wurden genutzt?</h2>
       <p class ="infotxt">Es wurden die Daten der <a href="https://www.mvv.de/">MVV</a> und des
       <a href="https://www.umweltbundesamt.de/">Umweltbundesamts</a>
       genutzt, diese wurden zu einem täglichen Maximalwert zusammengefasst.
       Es wurden die Daten der Letzten 3 Jahre verwendet.</p> 
 
-      <h2>Wie?</h2>
+      <h2>Wie benutze ich diese Seite?</h2>
       <p class ="infotxt">Zur einfacheren Bedienung gibt es in der oberen linken Ecke
       <strong on:mouseenter={activate} on:mouseleave={deactivate}
         >Buttons,</strong
@@ -209,6 +209,10 @@
       Entwicklungen zu ermöglichen.<br />
       Bei Auswahl einer Stadt wechselt dieser Text zu einem detailliertem Chart, der die
       Entwicklung im ausgewählten Zeitintervall anzeigt.</p>
+      <br />
+      <br />
+      <br />
+      <p><a href="https://github.com/Korbinian-Flietel/gdv_frontend">GitHub</a></p>
     </div>
     <p>{dates}</p>
     <p>{domain}</p>
